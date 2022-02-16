9c376d143287413bf251f4156e2e558d994cb035
alhexanderad <alhexanderad@gmail.com>
2022-02-16 18:09:09 -0500

    Se  realizo el grabado de hora de salida en la summary

--- student/views.py
+++ student/views.py
@@ -1,14 +1,11 @@
-from importlib.metadata import entry_points
-from typing import Any
 from django.urls import reverse
-from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_list_or_404
 from student.models import Apprentice, Assistance
 from datetime import datetime
 from student.forms import AssistanceForm
-from datetime import datetime, timedelta
-import time
+from datetime import datetime
+
 
 def get_into(request):
 
@@ -42,7 +39,7 @@ def get_into(request):
       data.exit_boolean = False
       data.save()
       print("To save")
-      return HttpResponseRedirect(reverse('student:list-apprentice',))
+      return HttpResponseRedirect(reverse('student:list_apprentice',))
   else:
     forms = AssistanceForm(request.POST or None, request.FILES or None)
   # try
@@ -71,25 +68,19 @@ def get_into(request):
 
 
 def list_apprentice(request):
-  obj = Apprentice.objects.all()
+  print("List de Aprendices")
+  # if request.method == "POST":
+  #   id = request.POST.getlist('assitance[]')
+  #   print("Numero de id: ",id)
+  #   return render(request, 'student/summary.html',)
+    # id_asst = Assistance.objects.get(pk=id)
+    # print("La salida es :", id_asst.exit_boolean)
+
   ass = Assistance.objects.all()
-  
-  app_1 = Assistance.objects.get(pk=2)
-  
-  print(app_1.entry_at)
-  
-  time_entry = app_1.entry_at.strftime('%H:%M')
-  time_exit = app_1.exit_at.strftime('%H:%M')
-  print("Entrada:", time_entry)
-  print ("Salida: ",time_exit)
-  formato = '%H:%M'
-  tiempo = datetime.strptime(time_exit, formato) - datetime.strptime(time_entry, formato)
-  #resultado = time_exit - time_entry
+
   context ={
     'title': "Lista",
-    'obj': obj,
     'ass' : ass,
-    'tiempo': tiempo,
   }
   return render(request, 'student/list.html', context)
 
@@ -98,9 +89,29 @@ def summary_assitance(request,pk,tm):
 #Una persona cuenta con un DNI, debe ser registrado su ingreso y salida.
 #Se realiza la busqueda la ultima vez que ingreso y que todavia no se registro su salida  
 #Se tiene la busqueda pk de aprentice la entry_at
+#Se debe de grabar summary
   app = Assistance.objects.filter(apprentice__id_app=pk,entry_at=tm,exit_boolean=False)
-
+  time_now= datetime.now()
+  
+  print(time_now)
   ass_id = Assistance.objects.get(pk=app[0].id_ass)
+
+  print(ass_id.id_ass)
+  if request.method == "POST":
+    ass_id.exit_boolean = True
+    ass_id.exit_at = time_now
+    ass_id.save(update_fields=['exit_boolean','exit_at'])
+    print("se grabo")
+    return HttpResponseRedirect(reverse('student:record_assitance',))
+    
+
+  #id_asst = Apprentice.objects.get(pk=int(ass_id.id_ass))
+
+    # id_asst = Assistance.objects.get(pk=id)
+    # print("La salida es :", id_asst.exit_boolean)
+    # time_now = datetime.now()
+    # print(time_now)
+    
   print(ass_id)
   context ={
     'ass_id':ass_id,
@@ -108,11 +119,10 @@ def summary_assitance(request,pk,tm):
     }
   return render(request, 'student/summary.html', context)
 
+
 def record_assitance(request):
 
   asst = Assistance.objects.filter(entry_boolean=True, exit_boolean=True)
-  
-  
   days_times_total =[]
   for n in asst:
     app_1 = Assistance.objects.get(pk=n.id_ass)
